a71e1a108137ed807f90dee733969504f1821388
georod <p.rodriguez97@gmail.com>
2021-05-07 18:09:39 -0400

    Edited R and csv files.

--- R_code/data_explore1.R
+++ R_code/data_explore1.R
@@ -6,6 +6,8 @@
 ### Aim:
 # Create a figure with the data I have gathered so far
 
+setwd("/home/peter/Documents/georod/carbon-seq-resources/R_code")
+
 ## libraries
 
 library(ggplot2)
@@ -14,9 +16,12 @@ d1 <- read.csv("./data_catalogue_v1.csv",header = TRUE, sep=',', quote="\"")
 str(d1)
 
 ## Forests 
-d2 <- d1[d1$habitat!='forests' & d1$use=='primary' & !is.na(d1$date_start),]
+d2 <- d1[d1$habitat!='wetlands' & d1$use=='primary' & !is.na(d1$date_start),]
 
 path1 <- c("../images/") 
+
+
+ggplot(d2) + geom_segment(aes(x=date_start,xend=ifelse(date_start==date_end, (date_end+0.5), date_end), y=short_name, yend=short_name, color=extent),size=10) + labs(x="Year") + labs(y = "") + ggtitle("Forest related data sets")
 
 png(paste0(path1,"forest_data_sets1.png"))
 ggplot(d2) + geom_segment(aes(x=date_start,xend=ifelse(date_start==date_end, (date_end+0.5), date_end), y=short_name, yend=short_name, color=extent),size=10) + labs(x="Year") + labs(y = "") + ggtitle("Forest related data sets")
