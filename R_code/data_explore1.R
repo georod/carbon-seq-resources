5fcc834c9d2dc1e9d66a55c0a16a8d5d40472027
georod <p.rodriguez97@gmail.com>
2021-05-14 16:43:04 -0400

    Edited table, R, and CSV file.

--- R_code/data_explore1.R
+++ R_code/data_explore1.R
@@ -1,6 +1,6 @@
 # Data exploration for thesis
 
-## 2021-05-04
+## 2021-05-14
 ## Peter R.
 
 ### Aim:
@@ -22,6 +22,7 @@ path1 <- c("../images/")
 
 
 ggplot(d2) + geom_segment(aes(x=date_start,xend=ifelse(date_start==date_end, (date_end+0.5), date_end), y=short_name, yend=short_name, color=extent),size=10) + labs(x="Year") + labs(y = "") + ggtitle("Forest related data sets")
+
 
 png(paste0(path1,"forest_data_sets1.png"))
 ggplot(d2) + geom_segment(aes(x=date_start,xend=ifelse(date_start==date_end, (date_end+0.5), date_end), y=short_name, yend=short_name, color=extent),size=10) + labs(x="Year") + labs(y = "") + ggtitle("Forest related data sets")
