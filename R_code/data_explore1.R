3069c84308e91f09898dbeb649fdd4124008300c
georod <p.rodriguez97@gmail.com>
2021-09-20 19:38:39 -0400

    Updated files

--- R_code/data_explore1.R
+++ R_code/data_explore1.R
@@ -6,7 +6,7 @@
 ### Aim:
 # Create a figure with the data I have gathered so far
 
-setwd("/home/peter/Documents/georod/carbon-seq-resources/R_code")
+setwd("~/Documents/georod/github/carbon-seq-resources/R_code")
 
 ## libraries
 
@@ -23,7 +23,7 @@ path1 <- c("../images/")
 
 ggplot(d2) + geom_segment(aes(x=date_start,xend=ifelse(date_start==date_end, (date_end+0.5), date_end), y=short_name, yend=short_name, color=extent),size=10) + labs(x="Year") + labs(y = "") + ggtitle("Forest related data sets")
 
-
+## Save as PNG
 png(paste0(path1,"forest_data_sets1.png"))
 ggplot(d2) + geom_segment(aes(x=date_start,xend=ifelse(date_start==date_end, (date_end+0.5), date_end), y=short_name, yend=short_name, color=extent),size=10) + labs(x="Year") + labs(y = "") + ggtitle("Forest related data sets")
 dev.off()
